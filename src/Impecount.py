15ba007764388a39dbf2cb1abfbb36ee1f3fbec8
Jesus Garcia <jesus.garcia1650@gmail.com>
2025-04-09 17:36:25 +0200

    Creado método para ejecutar poner la aplicación en ejecución

--- src/Impecount.py
+++ src/Impecount.py
@@ -2,7 +2,8 @@ from ImpecountController import ImpecountController
 
 def main():
     controller = ImpecountController()
-    controller.run()
+    controller.run_daily_pipeline()
+    # controller.run()
 
 if __name__ == "__main__":
     main()
