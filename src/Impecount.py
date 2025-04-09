85d99a8f0bfaa93ade7df285d62cb38e43e4a5e7
@JoseMS0508 <josemayasalas@gmail.com>
2025-04-09 04:18:25 +0200

    Creada clase Impecount.py

---
+++ src/Impecount.py
@@ -0,0 +1,8 @@
+from ImpecountController import ImpecountController
+
+def main():
+    controller = ImpecountController()
+    controller.run_daily_pipeline()
+
+if __name__ == "__main__":
+    main()
