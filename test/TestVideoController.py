33364421eaa187bb9c13bb6abe5680c31c7d9692
@JoseMS0508 <josemayasalas@gmail.com>
2025-04-10 19:06:22 +0200

    Modificamos codigo del test

--- test/TestVideoController.py
+++ test/TestVideoController.py
@@ -16,21 +16,21 @@ class TestVideoPersonDetection(unittest.TestCase):
     def test_video_with_people_detected(self):
         people_by_frame, processed_video = self.controller.count_people_frontal(self.video_with_people)
 
-        # Verifica si hay al menos un frame con personas
+        # Verificamos si hay al menos un frame con personas
         people_detected = any(count > 0 for _, count in people_by_frame)
         self.assertTrue(people_detected, "Se esperaba detección de personas, pero no se encontró ninguna.")
 
-        # Verifica que el vídeo procesado exista
+        # Verificamos que el vídeo procesado exista
         self.assertTrue(os.path.exists(processed_video.getPath()), "El vídeo procesado no se ha guardado correctamente.")
 
     def test_video_without_people_detected(self):
         people_by_frame, processed_video = self.controller.count_people_frontal(self.video_without_people)
 
-        # Verifica que todos los frames tengan conteo cero
+        # Verificamos que todos los frames tengan conteo cero
         all_frames_empty = all(count == 0 for _, count in people_by_frame)
         self.assertTrue(all_frames_empty, "No se esperaban personas, pero se detectó al menos una.")
 
-        # Verifica que el vídeo procesado exista
+        # Verificamos que el vídeo procesado exista
         self.assertTrue(os.path.exists(processed_video.getPath()), "El vídeo procesado no se ha guardado correctamente.")
 
 if __name__ == "__main__":
