7abc9f9c6f781dcd7df070cd63c4e91c0cf0687b
Jesus Garcia <jesus.garcia1650@gmail.com>
2025-04-10 10:43:32 +0200

    Creado modo test

--- src/Impecount.py
+++ src/Impecount.py
@@ -2,8 +2,7 @@ from ImpecountController import ImpecountController
 
 def main():
     controller = ImpecountController()
-    controller.run_daily_pipeline()
-    # controller.run()
+    controller.run()
 
 if __name__ == "__main__":
     main()
