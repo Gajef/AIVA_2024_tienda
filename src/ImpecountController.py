23cc35b8db6260c122d108e28751639d23b83b30
Jesus Garcia <jesus.garcia1650@gmail.com>
2025-04-10 16:18:01 +0200

    Código cambiado para usar la clase Video

--- src/ImpecountController.py
+++ src/ImpecountController.py
@@ -4,8 +4,7 @@ from VideoController import VideoController
 from CsvGeneratorController import CsvGeneratorController
 from EmailController import EmailController
 from datetime import datetime, timedelta, time
-import time
-
+import time as tm
 
 class ImpecountController:
     def __init__(self):
@@ -18,18 +17,18 @@ class ImpecountController:
 
     def run_daily_pipeline(self):
         print("[1] Obteniendo videos del servidor...")
-        videos_path = self.sftp.retrieve_last_videos()
+        last_videos = self.sftp.retrieve_last_videos()
         '''
         print("[2] Procesando video frontal...")
-        people_by_frame, processed_video_path = self.video_processor.count_people_frontal(videos_path[0])
-        videos_path.append(processed_video_path)'''
+        people_by_frame, processed_video_path = self.video_processor.count_people_frontal(last_videos[0])
+        last_videos.append(processed_video_path)'''
 
         print("[2.1] Procesando video lateral...")
-        people_by_frame, processed_video_path = self.video_processor.count_people_lateral(videos_path[1])
-        videos_path.append(processed_video_path)
+        people_by_frame, processed_video = self.video_processor.count_people_lateral(last_videos[1])
+        last_videos.append(processed_video)
 
         print("[3] Generando CSV...")
-        csv_path = self.csv_generator.generate_csv(people_by_frame)
+        generated_csv = self.csv_generator.generate_csv(people_by_frame)
 
         # Obtener la fecha del día anterior
         ayer = (datetime.now() - timedelta(days=1)).strftime("%d/%m/%Y")
@@ -38,7 +37,7 @@ class ImpecountController:
         status = self.email.send_email(
             subject="Informe diario de afluencia",
             body=f"Analítica del día {ayer}",
-            attachment_path=csv_path
+            attachment=generated_csv
         )
 
         if status == "OK":
@@ -47,25 +46,30 @@ class ImpecountController:
             print("⚠️ Hubo un problema al enviar el informe por email.")
 
 
-        #self.video_processor.delete_videos(videos_path)
+        # self.video_processor.delete_videos(last_videos)
         return "✅ Proceso completo"
 
     def run(self):
         while True:
             now_time = datetime.now()
+            retrieve_hour = time(hour=23, minute=0, second=0)
+            print("+-------------------------+\n|      ~ Impecount ~      |\n+-------------------------+")
             if self._test_mode:
-                retrieve_hour = now_time + timedelta(minutes=3)
-            else:
-                retrieve_hour = time(hour=23, minute=0, second=0)
-            if now_time.hour == retrieve_hour.hour and now_time.minute == retrieve_hour.minute:
+                print("[0] Aplicación iniciada en modo TEST")
                 self.run_daily_pipeline()
-                time.sleep(60)
+                print("[5] El modo prueba terminó")
+                break
             else:
-                time.sleep(30)
+                print("[0] Aplicación iniciada")
+                if now_time.hour == retrieve_hour.hour and now_time.minute == retrieve_hour.minute:
+                    self.run_daily_pipeline()
+                    tm.sleep(60)
+                else:
+                    tm.sleep(30)
 
     def _parse_args(self):
         parser = argparse.ArgumentParser()
         parser.add_argument("--email", dest="email", type=str, help="Receiver email", required=True)
-        parser.add_argument("--test", action="store_true", dest="test", help="Activate Test mode")
+        parser.add_argument("--test", action="store_true", dest="test_mode", help="Activate Test mode")
         args = parser.parse_args()
         return args
